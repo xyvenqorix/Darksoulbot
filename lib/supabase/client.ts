22756bc780611f83d344b39847d7b62ff3149d00
xyvenqorix <rodolfo.vercel@gmail.com>
2026-04-17 10:20:55 -0400

    client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL=https://dlfgyjpsoslnorwqcfga.supabase.co,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY=sb_publishable_6RYWbnctNBOF3qXv7p9wLw_mVFrZmsY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   )
 }
